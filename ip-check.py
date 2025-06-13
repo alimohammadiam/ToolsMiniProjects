4dc47f7d4ecdb017f57ca1f76a535b68c40c8ba7
Ali Mohammadi <ali0182mohammadi@gmail.com>
2025-06-13 16:17:49 +0330

    ip check

---
+++ ip-check.py
@@ -0,0 +1,11 @@
+import requests
+import ipapi
+
+myip = requests.get("http://api.ipify.org/").text
+
+ip_loc = ipapi.location(myip)['country_code']
+
+if ip_loc == 'IR':
+    print('blocked IP')
+else:
+    print('welcome')
